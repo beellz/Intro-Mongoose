efc29d1a28084a64c5ca4ef2cd0b6f4943568866
beellz <belal20795@gmail.com>
2020-05-19 00:08:48 +0530

    used update and removed functionality

--- app.js
+++ app.js
@@ -2,20 +2,27 @@ const mongoose = require('mongoose');
 mongoose.connect("mongodb://localhost:27017/fruitsDB", { useNewUrlParser: true, useUnifiedTopology: true});
 
 const fruitSchema = new mongoose.Schema ({
-    name: String,
-    rating: Number ,
+    name: {
+        type: String,
+        // required: [true, "Please check name entry"]
+    },
+    rating: {
+        type: Number,
+        min: 1,
+        max: 10
+    } ,
     review: String
 });
 
 const Fruit = mongoose.model("Fruit", fruitSchema);
 
 const fruit = new Fruit ({
-    name :"Apple",
-    rating: 7,
-    review: "awesome apple"
+    // name :"Apple",
+    rating: 6,
+    review: "peach peachy "
 });
 
-//  fruit.save();
+//   fruit.save();
 
 const personSchema = new mongoose.Schema ({
     name: String,
@@ -35,23 +42,23 @@ const person = new Person ({
 
 
 
-const kiwi = new Fruit({
-    name: "kiwi",
-    rating : 4 ,
-    review : "the best fruit!"
-});
+// const kiwi = new Fruit({
+//     name: "kiwi",
+//     rating : 4 ,
+//     review : "the best fruit!"
+// });
 
-const orange = new Fruit({
-    name: "orange",
-    rating : 5 ,
-    review : "type better way "
-});
+// const orange = new Fruit({
+//     name: "orange",
+//     rating : 5 ,
+//     review : "type better way "
+// });
 
-const banana = new Fruit({
-    name: "banana",
-    rating : 6 ,
-    review : "simple can eat without teeth"
-});
+// const banana = new Fruit({
+//     name: "banana",
+//     rating : 6 ,
+//     review : "simple can eat without teeth"
+// });
 
 
 // Fruit.insertMany([kiwi, orange ,banana], (err) => {
@@ -79,5 +86,22 @@ if (err) {
 });
 
 
+// Fruit.updateOne({_id: "5ec2d3cf0c491f04a68f06ed" }, {name: "Peach"},(err) => {
+//         if(err) {
+//             console.log(err);
+    
+//         } else {
+//             console.log("succesfully updated to the fruits to fruitDB");
+//         }
+//     } );
 
+
+Fruit.deleteOne({_id: "5ec2d3cf0c491f04a68f06ed" } , (err) => {
+    if(err) {
+        console.log(err);
+
+    } else {
+        console.log("succesfully deleted from the fruits to fruitDB");
+    }
+} );    
 
